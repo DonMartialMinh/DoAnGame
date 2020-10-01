46f70762ea98a1026ff7bf7111082171899c7194
fueledbyramental <43954674+fueledbyramental@users.noreply.github.com>
2020-10-01 07:35:53 +0700

    Add project files.

---
+++ DoAnGame/main.cpp
@@ -0,0 +1,240 @@
+/* =============================================================
+INTRODUCTION TO GAME PROGRAMMING SE102
+
+SAMPLE 00 - INTRODUCTORY CODE
+
+This sample illustrates how to:
+
+1/ Create a window
+2/ Initiate DirectX 9, Direct3D, DirectX Sprite
+3/ Draw a static brick sprite to the screen
+4/ Create frame rate independent movements
+
+5/ Some good C programming practices
+- Use constants whenever possible
+- 0 Warnings
+
+6/ Debug using __FILE__ __LINE__
+
+WARNING: This one file example has a hell LOT of *sinful* programming practices
+================================================================ */
+
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <vector>
+#include "Game.h"
+#include "GameObject.h"
+
+
+#define WINDOW_CLASS_NAME L"SampleWindow"
+#define WINDOW_TITLE L"00 - Intro"
+#define WINDOW_ICON_PATH L"brick.ico" 
+
+#define BRICK_TEXTURE_PATH L"brick.png"
+#define MARIO_TEXTURE_PATH L"mario.png"
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(0, 0, 0)
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+
+using namespace std;
+
+CMario* mario;
+#define MARIO_START_X 10.0f
+#define MARIO_START_Y 130.0f
+#define MARIO_START_VX 0.1f
+
+CGameObject* brick;
+#define BRICK_WIDTH 15.0f
+#define BRICK_X 0.0f
+#define BRICK_Y 155.0f
+
+float START_BRICK = BRICK_X;
+
+LPDIRECT3DTEXTURE9 texMario = NULL;
+LPDIRECT3DTEXTURE9 texBrick = NULL;
+
+vector<LPGAMEOBJECT> objects;
+
+
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+
+/*
+	Load game resources. In this example, we only load a brick image
+*/
+void LoadResources()
+{
+	CGame* game = CGame::GetInstance();
+	texBrick = game->LoadTexture(BRICK_TEXTURE_PATH);
+	texMario = game->LoadTexture(MARIO_TEXTURE_PATH);
+
+	mario = new CMario(MARIO_START_X, MARIO_START_Y, MARIO_START_VX, texMario);
+	
+	// Create a line of Brick under Mario
+	for (int i = 0; START_BRICK < SCREEN_WIDTH - BRICK_WIDTH; i++)
+	{
+		CGameObject* tBrick = new CGameObject(START_BRICK, BRICK_Y, texBrick);
+		objects.push_back(tBrick);
+		START_BRICK += BRICK_WIDTH;
+	}
+
+}
+
+/*
+	Update world status for this frame
+	dt: time period between beginning of last frame and beginning of this frame
+
+	IMPORTANT: no render-related code should be used inside this function.
+*/
+void Update(DWORD dt)
+{
+
+	mario->Update(dt);
+	for (int i = 0; i < objects.size(); i++)
+		objects[i]->Update(dt);
+}
+
+/*
+	Render a frame
+	IMPORTANT: world status must NOT be changed during rendering
+*/
+void Render()
+{
+	CGame* game = CGame::GetInstance();
+	LPDIRECT3DDEVICE9 d3ddv = game->GetDirect3DDevice();
+	LPDIRECT3DSURFACE9 bb = game->GetBackBuffer();
+	LPD3DXSPRITE spriteHandler = game->GetSpriteHandler();
+
+	if (d3ddv->BeginScene())
+	{
+		// Clear back buffer with a color
+		d3ddv->ColorFill(bb, NULL, BACKGROUND_COLOR);
+
+		spriteHandler->Begin(D3DXSPRITE_ALPHABLEND);
+
+
+		mario->Render();
+		for (int i = 0; i < objects.size(); i++)
+			objects[i]->Render();
+
+
+		spriteHandler->End();
+		d3ddv->EndScene();
+	}
+
+	// Display back buffer content to the screen
+	d3ddv->Present(NULL, NULL, NULL, NULL);
+}
+
+HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight)
+{
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hInstance = hInstance;
+
+	//Try this to see how the debug function prints out file and line 
+	//wc.hInstance = (HINSTANCE)-100; 
+
+	wc.lpfnWndProc = (WNDPROC)WinProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = (HICON)LoadImage(hInstance, WINDOW_ICON_PATH, IMAGE_ICON, 0, 0, LR_LOADFROMFILE);;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WINDOW_CLASS_NAME;
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd =
+		CreateWindow(
+			WINDOW_CLASS_NAME,
+			WINDOW_TITLE,
+			WS_OVERLAPPEDWINDOW, // WS_EX_TOPMOST | WS_VISIBLE | WS_POPUP,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			ScreenWidth,
+			ScreenHeight,
+			NULL,
+			NULL,
+			hInstance,
+			NULL);
+
+	if (!hWnd)
+	{
+		DWORD ErrCode = GetLastError();
+		//DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
+		return 0;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return hWnd;
+}
+
+int Run()
+{
+	MSG msg;
+	int done = 0;
+	DWORD frameStart = GetTickCount();
+	DWORD tickPerFrame = 1000 / MAX_FRAME_RATE;
+
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT) done = 1;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		DWORD now = GetTickCount();
+
+		// dt: the time between (beginning of last frame) and now
+		// this frame: the frame we are about to render
+		DWORD dt = now - frameStart;
+
+		if (dt >= tickPerFrame)
+		{
+			frameStart = now;
+			Update(dt);
+			Render();
+		}
+		else
+			Sleep(tickPerFrame - dt);
+	}
+
+	return 1;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hWnd = CreateGameWindow(hInstance, nCmdShow, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	CGame* game = CGame::GetInstance();
+	game->Init(hWnd);
+	LoadResources();
+	Run();
+
+	return 0;
+}
